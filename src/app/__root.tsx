0f122e24895069a701442b90e7cdf3b009c0cf92
RiskyMH <git@riskymh.dev>
2026-06-29 19:19:16 +1000

    ico icon

--- src/app/__root.tsx
+++ src/app/__root.tsx
@@ -41,12 +41,26 @@ export const Route = createRootRoute({
       // },
       {
         rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico',
+        sizes: "265x265"
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/honeypot.png',
+        sizes: "265x265"
+      },
+      {
+        rel: 'icon',
         type: 'image/svg+xml',
         href: '/honeypot.svg',
+        sizes: "any"
       },
       {
         rel: 'apple-touch-icon',
-        href: '/honeypot.svg',
+        href: '/honeypot.png',
+        sizes: "265x265"
       },
     ],
   }),
